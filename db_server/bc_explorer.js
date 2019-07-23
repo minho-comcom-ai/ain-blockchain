e85ed6c88d83f8c12c67c1df518ef16886d7be65
minsulee <minsulee0326@gmail.com>
2019-07-23 10:03:56 +0900

    Refactorying of query function

--- db_server/bc_explorer.js
+++ db_server/bc_explorer.js
@@ -32,8 +32,11 @@ class BlockchainExplorer {
 
     /**
      * Temporal function for test database.
+     * req, res, next
+     * queryString: query as string
+     * data as array
      */
-    createTable(req, res, next) {
+    query(req, res, next, queryString, data) {
         pg.connect(connectionString, function(err, client, done) {
             if(err){
                 done();
@@ -41,48 +44,16 @@ class BlockchainExplorer {
                 res.status(400).send(err);
             }
 
-            client.query(query0, (err, result) => {
+            client.query(queryString, data, (err, result) => {
                 if (err) {
                     console.log(err);
                     res.status(400).send(err);
                 }
-                console.log(result);
+                // console.log(result);
                 client.end();
             });
         });
     }
-
-    showData(req, res, next) {
-        pg.connect(connectionString, function(err, client, done) {
-            if(err){
-                done();
-                console.log(err);
-                res.status(400).send(err);
-            }
-            // const data = { id: "1", name: "abc", rollnumber: "11111" }; // dummy data for now.
-            // client.query('INSERT INTO Student(id, name, rollnumber) values($1, $2, $3)', [data.id, data.name, data.rollnumber]);
-
-            client.query('SELECT * FROM student where id = $1', [1], function(err, result) {
-                if (err) {
-                    console.log(err);
-                    res.status(400).send(err);
-                }
-                res.status(200).send(result.rows);
-            });
-         });
-    }
 }
-
-/**
- * Temporal place for Queries.
- */
-
- // Create block table.
-const query0 = `CREATE TABLE Blocks(height serial PRIMARY KEY,
-                                    timestamp int not null,
-                                    lasthash BYTEA NOT NULL,
-                                    hash BYTEA NOT NULL,
-                                    forger BYTEA not null,
-                                    threshold int not null);`;
 
 module.exports = BlockchainExplorer
--- db_server/index.js
+++ db_server/index.js
@@ -8,10 +8,27 @@ const app = express();
 const be = new BlockchainExplorer(TESTPORT);
 
 app.get('/', function (req, res, next) {
-    // be.createTable(req, res, next);
-    be.showData(req, res, next);
+    be.query(req, res, next, testQuery, [1]);
+    // be.query(req, res, next, testQuery, []);
 });
 
 app.listen(4000, function() {
     console.log("init localhost");
 });
+
+/**
+ * Temporal place for Queries.
+ */
+// Test Query Examples.
+// const testQuery = 'SELECT * FROM student';
+const testQuery = 'SELECT * FROM student where id = $1';
+// const data = { id: "1", name: "abc", rollnumber: "11111" }; // dummy data for now.
+// client.query('INSERT INTO Student(id, name, rollnumber) values($1, $2, $3)', [data.id, data.name, data.rollnumber]);
+
+// Create block table.
+const query0 = `CREATE TABLE Blocks(height serial PRIMARY KEY,
+                                    timestamp int not null,
+                                    lasthash BYTEA NOT NULL,
+                                    hash BYTEA NOT NULL,
+                                    forger BYTEA not null,
+                                    threshold int not null);`;
